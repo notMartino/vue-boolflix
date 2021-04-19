bc8c7052b54a912fc1470afd0328de75fdebf365
Martino Scalvini <martinoscalvinitobia@gmail.com>
2021-04-19 14:24:53 +0200

    Milestone 2: implementate bandiere lingua it/en, ricerca per serie TV

--- js/script.js
+++ js/script.js
@@ -7,19 +7,53 @@ function martinosflix() {
         el: '#vue',
         data: {
             search: '',
-            filmList: []
+            filmList: [],
+            serieList: [],
+            api_key: '159bee94cc034456f8348926b952865b'
         },
         methods: {
             searchFilm: function () {
                 if (this.search != '' && this.search[0] != ' ') {
                     console.log('Hai cercato: ' + this.search);
-                    axios.get('https://api.themoviedb.org/3/search/movie?api_key=159bee94cc034456f8348926b952865b&query=' + this.search).then(data => {
-                        console.log(data.data.results);
+
+                    // API ricerca film per nome
+                    axios.get('https://api.themoviedb.org/3/search/movie',
+                        {
+                            params:{
+                                api_key: this.api_key,
+                                query: this.search
+                            }
+                        }).then(data => {
+                        console.log(data);
                         this.filmList = data.data.results
-                        console.log(this.search);
+                        this.flagSrc 
+
+                    }).catch(() => {
+                        console.log('Error');
+                    });
+
+                    // API ricerca serie per nome
+                    axios.get('https://api.themoviedb.org/3/search/tv',
+                        {
+                            params:{
+                                api_key: this.api_key,
+                                query: this.search
+                            }
+                        }).then(data => {
+                        console.log(data);
+                        this.serieList = data.data.results
+                        this.flagSrc 
                     }).catch(() => {
                         console.log('Error');
                     })
+                    // axios.get('https://api.themoviedb.org/3/search/movie?api_key=159bee94cc034456f8348926b952865b&query=' + this.search).then(data => {
+                    //     console.log(data.data.results);
+                    //     this.filmList = data.data.results
+                    //     this.flagSrc 
+
+                    // }).catch(() => {
+                    //     console.log('Error');
+                    // })
                 }
             }
         }
