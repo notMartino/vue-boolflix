35fc355e41992ef0b423ccd97c07ea730f030af4
Martino Scalvini <martinoscalvinitobia@gmail.com>
2021-04-20 01:04:05 +0200

    Milestone 4: mockup on hover, mod. algoritmo stelle e lista film/serie

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-
 // Funzione princiapale
 function martinosflix() {
 
@@ -11,6 +9,28 @@ function martinosflix() {
             serieList: [],
             api_key: '159bee94cc034456f8348926b952865b',
             starClass: '',
+        },
+        computed:{
+            films: function () {
+                return this.filmList.map((film) =>{
+                    let stars = film.vote_average ;
+                    let rating = Math.round((5 * stars) / 10);
+
+                    let newFilm = {...film};
+                    newFilm.vote_average = rating;
+                    return newFilm;
+                });
+            },
+            series: function () {
+                return this.serieList.map((serie) =>{
+                    let stars = serie.vote_average ;
+                    let rating = Math.round((5 * stars) / 10);
+
+                    let newserie = {...serie};
+                    newserie.vote_average = rating;
+                    return newserie;
+                });
+            }
         },
         methods: {
             searchFilm: function () {
@@ -26,8 +46,8 @@ function martinosflix() {
                             }
                         }).then(data => {
                                 console.log('Films: ', data.data.results);
-                                this.filmList = data.data.results
-                                this.flagSrc 
+                                this.filmList = data.data.results;
+                               
                         }).catch(() => {
                             console.log('Error');
                     });
@@ -41,34 +61,28 @@ function martinosflix() {
                             }
                         }).then(data => {
                                 console.log('Serie TV: ', data.data.results);
-                                this.serieList = data.data.results
-                                this.flagSrc 
+                                this.serieList = data.data.results;
+                                
                         }).catch(() => {
                             console.log('Error');
                     });
-                    // axios.get('https://api.themoviedb.org/3/search/movie?api_key=159bee94cc034456f8348926b952865b&query=' + this.search).then(data => {
-                    //     console.log(data.data.results);
-                    //     this.filmList = data.data.results
-                    //     this.flagSrc 
-
-                    // }).catch(() => {
-                    //     console.log('Error');
-                    // })
                 }
-            },
-            starRating: function (film, index) {
-                let stars = film.vote_average;
-                let rating = Math.round((5 * stars) / 10);
-                let iconClass = '';
-
-                if(rating <= index) {
-                    iconClass = 'far fa-star';
-                }else{
-                    iconClass = 'fas fa-star';
-                }
-
-                return iconClass;
             }
+            // starRating: function (film) {
+            //     let stars = film.vote_average;
+            //     return rating = Math.round((5 * stars) / 10);
+                // return rating;
+
+                // let iconClass = '';
+
+                // if(rating <= index) {
+                //     iconClass = 'far fa-star';
+                // }else{
+                //     iconClass = 'fas fa-star';
+                // }
+
+                // return iconClass;
+            // }
         }
     });
 }
