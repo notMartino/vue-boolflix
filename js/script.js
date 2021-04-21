9d5968b399f8312be8f50728d6364329c616907c
Martino Scalvini <martinoscalvinitobia@gmail.com>
2021-04-21 17:46:07 +0200

    Refractoring codice, funzioni ottimizzate

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ function martinosflix() {
             nameList: [], // Lista nomi attori film/serie cliccata
             genreList: [], // Lista generi film/serie cliccata
             show: '', // classe display block
-            slicedWord: 50 // lunghezza stringa overview
+            slicedWord: 50, // lunghezza stringa overview
         },
         // Funzione film random/popolari
         mounted: function () {
@@ -24,7 +24,7 @@ function martinosflix() {
                         page: 1
                     }
                 }).then(data => {
-                        console.log('DATA: ', data.data.results);
+                        console.log('Popular: ', data.data.results);
                         this.popularFilm = data.data.results;
                 }).catch(() => {
                     console.log('Error');
@@ -33,83 +33,62 @@ function martinosflix() {
         computed:{
             // Generatore lista popular copiata, con stelle in base 5
             popular: function () {
-                return this.popularFilm.map((film) =>{
-                    let stars = film.vote_average ;
-                    let rating = Math.round((5 * stars) / 10);
-
-                    let newPopular = {...film};
-                    newPopular.vote_average = rating;
-                    return newPopular;
-                });
+                return this.arrayMap(this.popularFilm);
             },
             // Generatore lista films copiata, con stelle in base 5
             films: function () {
-                this.popularFilm = []; 
-                
-                return this.filmList.map((film) =>{
-                    let stars = film.vote_average ;
-                    let rating = Math.round((5 * stars) / 10);
-                    
-                    let newFilm = {...film};
-                    newFilm.vote_average = rating;
-                    return newFilm;
-                });
+                return this.arrayMap(this.filmList);
             },
             // Generatore lista serie copiata, con stelle in base 5
             series: function () {
-                this.popularFilm = [];
-                
-                return this.serieList.map((serie) =>{
-                    let stars = serie.vote_average ;
-                    let rating = Math.round((5 * stars) / 10);
-                    
-                    let newserie = {...serie};
-                    newserie.vote_average = rating;
-                    return newserie;
-                });
+                return this.arrayMap(this.serieList);
             }
         },
         methods: {
             // Funzione ricerca film/serie
-            searchFilm: function () {
+            searchFilm: function (type) {
                 if (this.search != '' && this.search[0] != ' ') {
-                    console.log('Hai cercato: ' + this.search);
-                    
-                    // API ricerca film per nome
-                    axios.get('https://api.themoviedb.org/3/search/movie',
+                    console.log("You've searcehd ("+ type + '): ' + this.search);
+                    // API ricerca film/serie per nome
+                    axios.get('https://api.themoviedb.org/3/search/' + type,
                     {
                         params:{
                             api_key: this.api_key,
                             query: this.search
                         }
                     }).then(data => {
-                        console.log('Films: ', data.data.results);
-                        this.filmList = data.data.results;
                         
-                    }).catch(() => {
-                        console.log('Error');
-                    });
-                    
-                    // API ricerca serie per nome
-                    axios.get('https://api.themoviedb.org/3/search/tv',
-                    {
-                        params:{
-                            api_key: this.api_key,
-                            query: this.search
+                        if (type == 'movie') {
+                            console.log('Films: ', data.data.results);
+                            this.filmList = data.data.results;
+                        }else{
+                            console.log('Series: ', data.data.results);
+                            this.serieList = data.data.results;
                         }
-                    }).then(data => {
-                        console.log('Serie TV: ', data.data.results);
-                        this.serieList = data.data.results;
-                        
+
+                        // if (this.filmList.length > 0 && this.serieList.length > 0) {
+                            this.popularFilm = []; 
+                        // }
                     }).catch(() => {
                         console.log('Error');
                     });
                 }
                 this.searched = true;
             },
-            actorsAndGenres: function (id) {
-                // API attori e generi per ID (film)
-                axios.get('https://api.themoviedb.org/3/movie/' + id, 
+            // Lista generica copiata, con stelle in base 5
+            arrayMap: function (arrayList) {
+                return arrayList.map((film) => {
+                    let stars = film.vote_average ;
+                    let rating = Math.round((5 * stars) / 10);
+
+                    let newItem = {...film};
+                    newItem.vote_average = rating;
+                    return newItem;
+                });
+            },
+            actorsAndGenres: function (id, type) {
+                // API attori e generi per ID
+                axios.get('https://api.themoviedb.org/3/' + type + '/' + id, 
                 {
                     params:{
                         api_key: this.api_key,
@@ -118,37 +97,6 @@ function martinosflix() {
                 }).then(data => {
                     if (this.nameList.length == 0) {
                         let axiosActors = data.data.credits.cast.slice(0,5);
-                            for (let i = 0; i < axiosActors.length; i++) {
-                                const element = axiosActors[i];
-                                this.nameList.push(element.original_name);
-                            }
-                        }
-
-                        if (this.genreList.length == 0) {
-                            let axiosGenres = data.data.genres;
-                            for (let i = 0; i < axiosGenres.length; i++) {
-                                const element = axiosGenres[i];
-                                this.genreList.push(element.name);
-                            }
-                        }
-                        
-                        this.show = 'show';
-                        this.slicedWord = 1000;
-                    }).catch(() => {
-                        console.log('Error');
-                    });
-            },
-            actorsAndGenresSerie: function (id) {
-                // API attori e generi per ID (serie)
-                axios.get('https://api.themoviedb.org/3/tv/' + id, 
-                    {
-                        params:{
-                            api_key: this.api_key,
-                            append_to_response: 'credits'
-                        }
-                    }).then(data => {
-                        if (this.nameList.length == 0) {
-                            let axiosActors = data.data.credits.cast.slice(0,5);
                             for (let i = 0; i < axiosActors.length; i++) {
                                 const element = axiosActors[i];
                                 this.nameList.push(element.original_name);
@@ -175,7 +123,7 @@ function martinosflix() {
                 this.nameList = [];
                 this.genreList = [];
                 this.slicedWord = 50;
-            },
+            }
         }
     });
 }
